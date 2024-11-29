dfd007c85aa37cafba3122f8251456048e1ba7b6
Dafne <147397064+dafnemorenop@users.noreply.github.com>
2024-11-29 15:04:31 +0100

    Update 3_EDA.py

--- pages/3_EDA.py
+++ pages/3_EDA.py
@@ -39,7 +39,7 @@ html_code = """
 
             display: flex;
             justify-content: flex-start;
-            margin-left: 1000px; 
+            margin-left: 700px; 
 
 
             position: fixed;
