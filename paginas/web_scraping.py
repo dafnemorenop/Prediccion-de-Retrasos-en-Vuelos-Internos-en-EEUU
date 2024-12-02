afaf1539a8cb16fcf372ea776e0a6f2d973720c8
Dafne <147397064+dafnemorenop@users.noreply.github.com>
2024-12-02 15:19:43 +0100

    Update web_scraping.py

--- paginas/web_scraping.py
+++ paginas/web_scraping.py
@@ -1,9 +1,5 @@
-import streamlit as st
-import requests
-from bs4 import BeautifulSoup
-from PIL import Image
-
 def display():
+
     # Título de la página web
     st.markdown(
         """
@@ -22,24 +18,39 @@ def display():
         """, unsafe_allow_html=True
     )
 
+
     # Descripción explicativa de cómo se realizó la extracción
+
+
+
     st.markdown("""
-        <div style="text-align: justify">
+            <div style="text-align: justify">
         <h3>Automatización de extracción de datos de DOT de USA</h3>
         
-        El proceso de extracción de datos de vuelos se realiza a través de un script automatizado que accede directamente al sitio web del **[Departamento de Estadísticas de Transporte de EE. UU.](https://www.transtats.bts.gov/ONTIME/Departures.aspx)**. Este script emplea librerías como BeautifulSoup y requests para extraer la información sobre vuelos internos, retrasos y otros datos relevantes para el análisis de la puntualidad aérea en los Estados Unidos.
+        El proceso de extracción de datos de vuelos se realiza a través de un script automatizado que accede directamente al sitio web del **[Departamento de Estadísticas de Transporte de EE. UU.](https://www.transtats.bts.gov/ONTIME/Departures.aspx)**. Este script emplea librerías como BeautifulSoup y Selenium para navegar en el sitio, seleccionar las opciones de aeropuertos y aerolíneas, y extraer la información sobre vuelos internos, retrasos y otros datos relevantes para el análisis de la puntualidad aérea en los Estados Unidos.
 
+    
         <h3>Flujo de trabajo</h3>
         1. **Obtención de opciones de aeropuertos y aerolíneas**:
         - Se hace una solicitud HTTP a la página y se extraen los datos de los menús desplegables (aeropuertos y aerolíneas) utilizando la librería `BeautifulSoup`.
 
-        2. **Descarga de archivos CSV**:
-        - Una vez seleccionados los valores deseados, se simula el envío del formulario para generar y descargar los archivos CSV de los datos de vuelos.
+        2. **Automatización de la selección de datos con Selenium**:
+        - Se emplea Selenium para iniciar un navegador y seleccionar las opciones de aeropuertos y aerolíneas de manera programada.
+
+        3. **Preselección de estadísticas y configuraciones**:
+        - Se marcan las opciones de estadísticas, días, meses y años para ajustar la búsqueda.
+
+        4. **Iteración para extraer los datos**:
+        - Se recorren todas las combinaciones de aeropuertos y aerolíneas para hacer clic en el botón de descarga y guardar los archivos CSV.
+
+        5. **Manejo de errores y estado de los datos**:
+        - Se implementa un manejo de excepciones para casos donde no se encuentran datos o el proceso falla, notificando al usuario de manera apropiada.
+        
         </div>""", unsafe_allow_html=True)
 
     st.markdown("""
         <div style="padding: 10px; border: 1px solid #f5c6cb; background-color: #721c24; border-radius: 5px;">
-            <strong style="color: white;">Advertencia:</strong> Al pulsar el siguiente botón, se iniciará el proceso de extracción de datos de forma automatizada. Este proceso puede tomar un tiempo dependiendo de la cantidad de datos.
+            <strong style="color: white;">Advertencia:</strong> Si desea los datos para trabajar con ellos podrá extraerlos corriendo el notebook de 'notebooks\01.0_web_scraping.ipynb' en github. Tenga en cuenta que se iniciará el proceso de extracción de datos de forma automatizada, lo que descargará más de 2000 archivos CSV. Este proceso puede tomar varias horas y generar una gran cantidad de datos, lo que podría afectar el rendimiento de tu dispositivo o conexión. Asegúrate de tener suficiente espacio de almacenamiento y una conexión estable antes de proceder.
         </div>
     """, unsafe_allow_html=True)
 
@@ -67,79 +78,11 @@ def display():
         </style>
     """, unsafe_allow_html=True)
 
-    # Botón estilizado y centrado
-    if st.button('Iniciar extracción', use_container_width=True, type="primary"):
-        st.write("Iniciando el proceso de extracción de datos...")
-
-        url = "https://www.transtats.bts.gov/ONTIME/Departures.aspx"
-
-        def obtener_opciones(url, aeropuertos_aerolineas):
-            """Con esta función se seleccionan los aeropuertos o las aerolíneas que están en un desplegable"""
-            response = requests.get(url)
-            soup = BeautifulSoup(response.text, 'html.parser')
-
-            seleccionar_aeropuerto_aerolinea = soup.find("select", attrs={"name": aeropuertos_aerolineas})
-            if seleccionar_aeropuerto_aerolinea:
-                opciones = seleccionar_aeropuerto_aerolinea.find_all("option")
-                listado_opciones = [opcion['value'] for opcion in opciones if opcion['value'] != '']
-                return listado_opciones
-            else:
-                return []
-
-        # Obtener listado de aeropuertos y aerolíneas
-        listado_aeropuertos = obtener_opciones(url, "cboAirport")
-        listado_aerolineas = obtener_opciones(url, "cboAirline")
-
-        # Comprobar si se encontraron los datos necesarios
-        if not listado_aeropuertos or not listado_aerolineas:
-            st.error("No se pudieron obtener los aeropuertos o las aerolíneas de la página.")
-        else:
-            st.write(f"Se han encontrado {len(listado_aeropuertos)} aeropuertos y {len(listado_aerolineas)} aerolíneas.")
-
-            # Aquí comenzamos la descarga de archivos CSV para cada aeropuerto y aerolínea
-            for aeropuerto in listado_aeropuertos:
-                for aerolinea in listado_aerolineas:
-                    st.write(f"Descargando datos para aeropuerto {aeropuerto} y aerolínea {aerolinea}...")
-
-                    # Crear los datos de la solicitud POST (simulando el formulario)
-                    data = {
-                        'cboAirport': aeropuerto,  # Aeropuerto seleccionado
-                        'cboAirline': aerolinea,  # Aerolínea seleccionada
-                        'chkAllStatistics': 'on',  # Marcar la opción para todas las estadísticas
-                        'chkAllDays': 'on',  # Marcar la opción para todos los días
-                        'chkMonths_11': 'on',  # Diciembre (Mes 11)
-                        'chkYears_34': 'on',  # 2021
-                        'chkYears_35': 'on',  # 2022
-                        'chkYears_36': 'on',  # 2023
-                    }
-
-                    # Hacer una solicitud POST para generar el CSV
-                    response = requests.post(url, data=data)
-
-                    # Si la solicitud es exitosa y se genera el archivo CSV, lo descargamos
-                    if response.status_code == 200:
-                        # Buscar el enlace de descarga del CSV en la respuesta
-                        soup = BeautifulSoup(response.text, 'html.parser')
-                        download_link = soup.find('a', id='DL_CSV')  # Identificamos el enlace de descarga
-
-                        if download_link:
-                            csv_url = download_link['href']
-                            st.write(f"Descargando archivo CSV desde: {csv_url}")
-
-                            # Descargar el archivo CSV
-                            csv_response = requests.get(csv_url)
-                            if csv_response.status_code == 200:
-                                file_name = f"datos_{aeropuerto}_{aerolinea}.csv"
-                                with open(file_name, 'wb') as file:
-                                    file.write(csv_response.content)
-                                st.write(f"Archivo descargado: {file_name}")
-                            else:
-                                st.write("No se pudo descargar el archivo CSV.")
-                    else:
-                        st.write("Error al generar los datos CSV.")
-
     image = Image.open('images/avion.jpg')
+
+
     st.image(image, use_container_width=True)
+
 
     # Copos de nieve
     st.markdown(
